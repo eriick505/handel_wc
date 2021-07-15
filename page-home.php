0eb8c1d34ffd698f20da216fdd50e034b9cdf581
unknown <santoserick@gmail.com>
2021-07-15 13:34:52 -0300

    Template inicial

---
+++ page-home.php
@@ -0,0 +1,15 @@
+<?php
+// Template name: Home
+
+get_header(); 
+?>
+
+
+<?php if(have_posts()) { while(have_posts()) { the_post(); ?>
+
+<h1><?php the_title(); ?></h1>
+<p><?php the_content(); ?></p>
+
+<?php } } ?>
+
+<?php get_footer(); ?>
