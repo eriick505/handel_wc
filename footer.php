9611c225d70be928be52affaa72416160c032e9d
unknown <santoserick@gmail.com>
2021-07-17 00:37:15 -0300

    adicionado a importação do script

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 
   <?php wp_footer(); ?>
+
+  <script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/slide.js"></script>
+  <script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/script.js"></script>
 </body>
 </html>
