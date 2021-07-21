0b63b05353521251d741212d1fcd1fbbe8f17293
Erick <santoserick9@gmail.com>
2021-07-21 14:07:56 -0300

    adicionado filtro que remove classes do woocommerce de algumas paginas

--- functions.php
+++ functions.php
@@ -6,6 +6,21 @@ function handel_add_woocommerce_support() {
 }
 add_action('after_setup_theme', 'handel_add_woocommerce_support');
 
+// Remove algumas classes do body adicionadas por padrão pelo woocommerce 
+function remove_some_body_class($classes) {
+  $woo_class = array_search('woocommerce', $classes);
+  $woopage_class = array_search('woocommerce-page', $classes);
+  $search = in_array('archive', $classes) || in_array('product-template-default', $classes);
+
+  if($woo_class && $woopage_class && $search) {
+    unset($classes[$woo_class]);
+    unset($classes[$woopage_class]);
+  }
+
+  return $classes;
+}
+add_filter('body_class', 'remove_some_body_class');
+
 // Registra o css inicial do tema
 function handel_css() {
 	wp_register_style('handel-style', get_template_directory_uri() . '/style.css', array(), '1.0.0', false);
