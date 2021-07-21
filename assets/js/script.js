370b665d9bb747ee88de201fa474b8fc663fb204
Erick <santoserick9@gmail.com>
2021-07-21 17:45:13 -0300

    Criado script para validar o campo de busca antes de ser enviado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
-const slide = new Slide('.slide', '.slide-wrapper');
+function initHandleSearchInputSubmit() {
+  const searchFormEl = document.querySelector("#searchForm");
+  const inputSearchFormEl = searchFormEl.querySelector("#s");
+
+  function showBoxMessage() {
+    const boxMessage = document.createElement("div");
+    boxMessage.classList.add("boxWarning");
+    boxMessage.textContent = "Você precisa digitar algo antes de pesquisar.";
+
+    boxMessage.animate(
+      [
+        { transform: "translateX(-20px)", opacity: 0 },
+        { transform: "initial", opacity: 1 },
+      ],
+      {
+        duration: 100,
+      }
+    );
+
+    inputSearchFormEl.insertAdjacentElement("afterend", boxMessage);
+
+    setTimeout(() => {
+      boxMessage.remove();
+    }, 1500);
+  }
+
+  function handleSearchForm(e) {
+    if (inputSearchFormEl.value === "") {
+      e.preventDefault();
+      showBoxMessage();
+      return;
+    }
+
+    searchFormEl.submit();
+  }
+
+  searchFormEl.addEventListener("submit", handleSearchForm);
+}
+
+const slide = new Slide(".slide", ".slide-wrapper");
 
 slide.init();
+initHandleSearchInputSubmit();
