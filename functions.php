01d8f127c050118a71a723a8acbd1713833e4530
Erick <santoserick9@gmail.com>
2021-07-24 12:18:34 -0300

    movida as funções de lidar com os dados do produto para um outro arquivo chamado 'format-product' e adicionado o include do arquivo, adicionado o include de um arquivo que modifica o menu da pagina 'minha conta'

--- functions.php
+++ functions.php
@@ -68,42 +68,10 @@ function multiple_widget_init(){
 add_action('widgets_init', 'multiple_widget_init');
 
 
-// Função que formata os dados dos produtos 
-function format_products($products, $img_size = 'medium') {
-  $products_final = [];
+// Função para formatação dos dados do produto e template de lista de produto
+include(get_template_directory() . '/inc/format-product.php');
 
-  foreach($products as $product) {
-    $products_final[] = [
-      'id' => $product->get_id(),
-      'name' => $product->get_name(),
-      'price' => $product->get_price_html(),
-      'link' => $product->get_permalink(),
-      'img' => wp_get_attachment_image_src($product->get_image_id(), $img_size)[0],
-    ];
-  }
-
-  return $products_final;
-}
-
-// Função que cria o template de lista de produtos
-function handel_product_list($products)  { ?>
-	<ul class="products-list">
-    <?php foreach($products as $product) { ?>
-      <li class="product-item">
-        <a href="<?= $product['link']; ?>">
-          <div class="product-info">
-            <img src="<?= $product['img']; ?>" alt="<?= $product['name']; ?>">
-            <h2><?= $product['name']; ?> - <span><?= $product['price']; ?></span></h2>
-          </div>
-          <div class="product-overlay">
-            <span class="btn-link">Ver Mais</span>
-          </div>
-        </a>
-      </li>
-    <?php } ?>
-  </ul>
-<?php 
-} // fecha a função handel_product_list
+include(get_template_directory() . '/inc/user-custom-menu.php');
 
 ?>
 
